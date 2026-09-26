f9e43dfbe94b2a80b434a40bd15fff2ad755c9cf
praveensk7982-stack <praveen.sk.7982@gmail.com>
2026-09-26 19:04:45 +0530

    updated project

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,13 +1,18 @@
-import admin from 'firebase-admin';
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-  });
-}
+const apps = getApps();
+
+const app = apps.length
+  ? apps[0]
+  : initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    });
+
+const admin = { auth: () => getAuth(app) };
 
 export default admin;

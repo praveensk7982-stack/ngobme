600d435a72158c90cfded20fa64d46c994f36cd0
praveensk7982-stack <praveen.sk.7982@gmail.com>
2026-09-26 14:43:32 +0530

    updated project

--- api/send-otp.js
+++ api/send-otp.js
@@ -35,16 +35,11 @@ export default async function handler(req, res) {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: process.env.SMTP_SECURE === 'true',
+      service: 'gmail',
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
-      },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000
+      }
     });
 
     await transporter.sendMail({
